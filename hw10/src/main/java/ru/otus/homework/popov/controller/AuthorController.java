3c713a26e2f277f94e85b9325968985ab0bb078f
Alexander Popov <as-popov@yandex.ru>
2022-05-06 22:21:18 +0300

    AuthorController, GenreController

---
+++ hw10/src/main/java/ru/otus/homework/popov/controller/AuthorController.java
@@ -0,0 +1,22 @@
+package ru.otus.homework.popov.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.otus.homework.popov.domain.Author;
+import ru.otus.homework.popov.service.AuthorOperations;
+
+import java.util.List;
+
+@RestController
+public class AuthorController {
+    private final AuthorOperations authorOperations;
+
+    public AuthorController(AuthorOperations authorOperations) {
+        this.authorOperations = authorOperations;
+    }
+
+    @GetMapping("/authors")
+    public List<Author> getAuthors() {
+        return authorOperations.findAll();
+    }
+}
